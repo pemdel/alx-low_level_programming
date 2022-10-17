0b259ae402cbe15d79f2a4d13a3fe3363d104574
pemdel <favourogundele4@gmail.com>
2022-10-17 08:08:28 -0700

    m32 m64

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* my commenting starts here */
+int main(void)
+{
+/* i will get the code to print sizes of datatypes */
+	printf("Size of a char: %d byte(s) \n", sizeof(char));
+	printf("Size of an int: %d byte(s) \n", sizeof(int));
+	printf("Size of a long int: %d byte(s) \n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s) \n", sizeof(long long int));
+	printf("Size of a float: %d byte(s) \n", sizeof(float));
+	return (0);
+}
