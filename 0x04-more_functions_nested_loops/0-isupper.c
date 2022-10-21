c86237576bef8b3f0cccec9441214ac45fcbeaf7
pemdel <favourogundele4@gmail.com>
2022-10-20 18:03:07 -0700

    uppercase letters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase letters
+ * @c: char to check
+ *
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && C <= 'Z')
+		return (1);
+	else
+		return (0);
+}
