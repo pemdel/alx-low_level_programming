aee07ccf993c800df25356f50d50531169ea4421
pemdel <favourogundele4@gmail.com>
2022-10-26 15:31:04 -0700

    comare \'em

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: the differnce in s1 and s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int y;
+
+	y = 0;
+	while (s1[y] != '\0' && s2[y] != '\0')
+	{
+		if (s1[y] != s2[y])
+		{
+			return (s1[y] - s2[y]);
+		}
+	y++;
+	}
+	return (0);
