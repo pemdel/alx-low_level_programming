b02ddb6248e6bcce86115e7b0c41e339c0ceb009
pemdel <favourogundele4@gmail.com>
2022-10-27 18:14:19 -0700

    theres a warning

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes words
+ * @str: string
+ * Return: pointer to the string
+ */
+
+char *cap_string(char *str)
+{
+	int y = 0;
+
+	while (str[y])
+	{
+		while (!(str[y] >= 'a' && str[y] <= 'z'))
+			y++;
+		if (str[y - 1] == ' ' || str[y - 1] == '\t' || str[y - 1] == '\n' || str[y - 1] == ',' || str[y - 1] == ';' || str[y - 1] == '.' || str[y - 1] == '!' || str[y - 1] == '?' || str[y - 1] == '"' || str[y - 1] == '(' || str[y - 1] == ')' || str[y - 1] == '{' || str[y - 1] == '}' || y == 0)
+			str[y] -= 32;
+		y++;
+	}
+	return (str);
+}
