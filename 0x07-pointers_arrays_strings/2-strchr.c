0e83f9cb45811ec14facf3bfe48829b9188b5a88
pemdel <favourogundele4@gmail.com>
2022-10-31 17:31:23 -0700

    finding a char

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr -  locates a character in a string
+ * @s: str given
+ * @c: char given
+ * Return: a string
+ */
+
+char *_strchr(char *s, char c)
+{
+	int a = 0, b;
+
+	while (s[a])
+		a++;
+	for (b = 0; b < a; b++)
+	{
+		if (c == s[b])
+			s += b;
+		return (s);
+	}
+	return ('\0');
+}
