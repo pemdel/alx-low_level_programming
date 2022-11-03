992cf9feb4191a97701576fcad9abbf29294470f
pemdel <favourogundele4@gmail.com>
2022-11-02 19:23:34 -0700

    just testing

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *  _sqrt_recursion - square roots using recursion
+ * @n: parameter to square root
+ * Return: square root of a number
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
