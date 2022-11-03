73cd525da996e7a6f9969a209b2c1db85e3100d3
pemdel <favourogundele4@gmail.com>
2022-11-02 19:40:29 -0700

    this should be it

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int _sqrt(int, int);
+
 /**
  *  _sqrt_recursion - square roots using recursion
  * @n: parameter to square root
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * is_prime_number - identifies prime numbers
+ *
+ *
+ */
+
+int is_prime_number(int n)
+{
