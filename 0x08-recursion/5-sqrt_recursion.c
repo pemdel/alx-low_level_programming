9d8ef19f777194807588125c1ddc5b648a399d72
pemdel <favourogundele4@gmail.com>
2022-11-02 19:32:36 -0700

    having two functions in a question

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,3 +10,21 @@ int _sqrt_recursion(int n)
 {
 	return (_sqrt(n, 1));
 }
+
+/**
+ * _sqrt - recursive square root
+ * @n: number
+ * @i: iterator
+ * Return: a number
+ */
+
+int _sqrt(int n, int i)
+{
+	int square = i * i;
+
+	if (square > n)
+		return (-1);
+	if (square == n)
+		return (i);
+	return (_sqrrt(n, i + 1));
+}
