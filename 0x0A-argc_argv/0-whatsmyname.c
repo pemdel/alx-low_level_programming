1433545f7056e8f8170153cd468494766bb949db
pemdel <favourogundele4@gmail.com>
2022-11-04 21:03:39 -0700

    printing of name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is a function
+ * @argc: argc parameter
+ * @argv: an array of a listed comment
+ * Return: 0 for success.
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
