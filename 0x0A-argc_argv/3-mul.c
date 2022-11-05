0b877041cd817c00de5dbf063f15b21e99c7a871
pemdel <favourogundele4@gmail.com>
2022-11-04 22:03:28 -0700

    this is getting annoying'

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -17,5 +17,9 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	num1 = atoi(argv[0]);
-	num2 = argv[2];
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+	return (0);
 }
